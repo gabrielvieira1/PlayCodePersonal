dee455c01faecc8988e5ce0147e61f6af58f0a8f
gabrielvieira1 <gabrielvieira1@protonmail.com>
2020-06-15 16:11:24 -0300

    Project started

---
+++ index.js
@@ -0,0 +1,16 @@
+const test = {
+  opa: {
+    eita: {
+      ola: {
+        teste: "Já chegou o disco voador"
+      }
+    }
+  }
+}
+
+function testando() {
+  return 'Eita bixiga';
+}
+
+console.log(testando());
+console.log(test.opa.eita.ola.teste);
